b2ffd142c0c97b745edb94df71b453487fbd3ae3
surekha <surekhahosamani333@gmail.com>
2024-04-16 23:36:55 +0530

    first commit

---
+++ src/main/java/mypkg/Mycalculator.java
@@ -0,0 +1,20 @@
+package mypkg;
+public class Mycalculator {
+	public int sum(int a ,int b)
+	{
+		return a+b;
+		
+	}
+	public int diff(int a ,int b)
+	{
+		return a-b;
+	}
+	public static void main(String[] args) {
+		
+		Mycalculator calc = new Mycalculator();
+		System.out.println("Sum is :"+calc.sum(20, 10));
+		System.out.println("Diff is :"+calc.diff(20, 10)) ;
+		
+	}
+
+}
